12fe7277829d9a81d722d2ff88ce684ed813c567
Droski0802 <pedromanuelwork@gmail.com>
2024-10-17 15:36:26 -0400

    Basic creation of the Users microservice

---
+++ src/Services/Users/Users.Application/UsersApli.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Application
+{
+    internal class UsersApli
+    {
+    }
+}
