3df455db3f5b91eb3cf1e99d135711417489b0a1
Droski0802 <pedromanuelwork@gmail.com>
2024-10-29 14:30:41 -0400

    Creacion e implementacion de los comandos y handlers (update y delete) de drivers

---
+++ src/Services/Providers/Providers.Application/Drivers/Commands/DeleteDriver/DeleteDriverCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Providers.Application.Drivers.Commands.DeleteDriver;
+
+public record DeleteDriverCommand(Guid DriverId) : ICommand<DeleteDriverResult>;
+
+public record DeleteDriverResult(bool IsSuccess);
+
+public class DeleteDriverCommandValidator : AbstractValidator<DeleteDriverCommand>
+{
+    public DeleteDriverCommandValidator()
+    {
+        RuleFor(x => x.DriverId).NotEmpty().WithMessage("DriverId is required");
+    }
+}
---
+++ src/Services/Providers/Providers.Application/Drivers/Commands/DeleteDriver/DeleteDriverHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Providers.Application.Drivers.Commands.DeleteDriver;
+
+public class DeleteDriverHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteDriverCommand, DeleteDriverResult>
+{
+    public async Task<DeleteDriverResult> Handle(DeleteDriverCommand command, CancellationToken cancellationToken)
+    {
+        var driver = await dbContext.Drivers
+            .FindAsync([command.DriverId], cancellationToken)
+            ?? throw new DriverNotFoundException(command.DriverId);
+
+        dbContext.Drivers.Remove(driver);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteDriverResult(true);
+    }
+}
