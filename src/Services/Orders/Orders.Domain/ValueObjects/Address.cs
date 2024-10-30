40e091d2bdadcfe32239bb2944b18dc82b7b5657
Droski0802 <pedromanuelwork@gmail.com>
2024-10-30 15:08:44 -0400

    Cambios en el dominio del microservicio de Ordenes v7

---
+++ src/Services/Orders/Orders.Domain/ValueObjects/Address.cs
@@ -0,0 +1,30 @@
+﻿namespace Orders.Domain.ValueObjects;
+
+public record Address
+{
+    public string AddressLine1 { get; } = default!;
+    public string AddressLine2 { get; } = default!;
+    public string City { get; } = default!;
+    public string State { get; } = default!;
+    public string Zip { get; } = default!;
+
+    private Address(string address1, string address2, string city, string state, string zip) 
+    {
+        AddressLine1 = address1;
+        AddressLine2 = address2;
+        City = city;
+        State = state;
+        Zip = zip;
+    }
+
+    public static Address Of(string address1, string address2, string city, string state, string zip)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(address1);
+        ArgumentException.ThrowIfNullOrWhiteSpace(address2);
+        ArgumentException.ThrowIfNullOrWhiteSpace(city);
+        ArgumentException.ThrowIfNullOrWhiteSpace(state);
+        ArgumentException.ThrowIfNullOrWhiteSpace(zip);
+
+        return new Address(address1,address2,city,state,zip);
+    }
+}
---
+++ src/Services/Orders/Orders.Domain/ValueObjects/Price.cs
@@ -0,0 +1,21 @@
+﻿namespace Orders.Domain.ValueObjects;
+
+public record Price
+{
+    public int AnnualPrice { get; } = default!;
+    public int MonthlyPrice { get; } = default!;
+
+    private Price(int annualPrice, int monthlyPrice)
+    {
+        AnnualPrice = annualPrice;
+        MonthlyPrice = monthlyPrice;
+    }
+
+    public static Price Of( int annualPrice, int monthlyPrice)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(annualPrice.ToString());
+        ArgumentException.ThrowIfNullOrWhiteSpace(monthlyPrice.ToString());
+
+        return new Price(annualPrice, monthlyPrice);
+    }
+}
