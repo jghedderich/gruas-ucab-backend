77b68a85ea8d92cffc5deb5c1a5f49f9ee4de181
Droski0802 <pedromanuelwork@gmail.com>
2024-10-31 18:26:27 -0400

    Creacion del OrderConfiguration en el microservicio de Ordenes, Orders.Infrastructure

--- src/Services/Orders/Orders.Infrastructure/Data/Configuration/OrderConfiguration.cs
+++ src/Services/Orders/Orders.Infrastructure/Data/Configuration/OrderConfiguration.cs
@@ -47,5 +47,37 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
                     vehicleType => (VehicleType)Enum.Parse(typeof(VehicleType), vehicleType));
             });
         });
+
+        builder.ComplexProperty(o => o.OrderStatus, orderStatusBuilder =>
+        {
+            orderStatusBuilder.Property(os => os.Status)
+                .HasConversion(os => os.ToString(),
+                status => (Status)Enum.Parse(typeof(Status), status));
+        });
+
+        builder.ComplexProperty(o => o.IncidentAddress, incidentAddressBuilder =>
+        {
+            incidentAddressBuilder.Property( ia => ia.AddressLine1).HasMaxLength(500).IsRequired();
+            incidentAddressBuilder.Property( ia => ia.AddressLine2).HasMaxLength(500);
+            incidentAddressBuilder.Property( ia => ia.City).HasMaxLength(50).IsRequired();
+            incidentAddressBuilder.Property( ia => ia.State).HasMaxLength(50).IsRequired();
+            incidentAddressBuilder.Property(ia => ia.Zip).HasMaxLength(4).IsRequired();
+        });
+
+        builder.ComplexProperty(o => o.DestinationAddress, destinationAddresBuilder =>
+        {
+            destinationAddresBuilder.Property(da => da.AddressLine1).HasMaxLength(500).IsRequired();
+            destinationAddresBuilder.Property(da => da.AddressLine2).HasMaxLength(500);
+            destinationAddresBuilder.Property(da => da.City).HasMaxLength(50).IsRequired();
+            destinationAddresBuilder.Property(da => da.State).HasMaxLength(50).IsRequired();
+            destinationAddresBuilder.Property(da => da.Zip).HasMaxLength(4).IsRequired();
+        });
+
+        builder.OwnsMany(o => o.AdditionalCost, additionalCostBuilder =>
+        {
+            additionalCostBuilder.Property(a => a.Description).HasMaxLength(300).IsRequired();
+            additionalCostBuilder.Property(a => a.Amount).HasConversion(v => ToString(), v => v != null ? decimal.Parse(v) : 0).IsRequired();
+            additionalCostBuilder.Property(a => a.IsApproved).HasConversion(v => v ? 1 : 0, v => v == 1);
+        });
     }
 }
