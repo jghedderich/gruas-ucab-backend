a3b420df06b7f8590729c26d0600828e29fa07af
Pedro Manuel Rodriguez <134349337+Droski0802@users.noreply.github.com>
2024-10-31 17:45:50 -0400

    Fixes en Dominio: Modelos y ValueObject - Desarrollo de Orders.Infras… (#22)
    * Fixes en Dominio: Modelos y ValueObject - Desarrollo de Orders.Infrastructure: Data (Configuration)
    
    * Fixes en Dominio: Modelos y ValueObject - Desarrollo de Orders.Infrastructure: Data (Configuration)
    
    ---------
    
    Co-authored-by: Droski0802 <pedromanuelwork@gmail.com>

---
+++ src/Services/Orders/Orders.Infrastructure/Data/Configuration/OperatorConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Orders.Domain.Models;
+
+namespace Orders.Infrastructure.Data.Configuration;
+
+public class OperatorConfiguration : IEntityTypeConfiguration<Operator>
+{
+    public void Configure(EntityTypeBuilder<Operator> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        // Operator has many orders
+        builder.HasMany(a => a.Orders)
+            .WithOne()
+            .HasForeignKey(o => o.OperatorId);
+    }
+}
