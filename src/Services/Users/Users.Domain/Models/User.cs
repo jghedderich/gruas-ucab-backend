de903e5e1619c0f84a4f4f8c1532aaa4ba27d3bf
Juan Hedderich <hedderich.dev@gmail.com>
2024-11-07 13:16:58 -0400

    Create user domain for authentication and authorization

---
+++ src/Services/Users/Users.Domain/Models/User.cs
@@ -0,0 +1,52 @@
+﻿
+using Users.Domain.Events;
+using Users.Domain.ValueObjects;
+
+namespace Users.Domain.Models; 
+public class User : Aggregate<Guid>
+{
+    public Name Name { get; private set; } = default!;
+    public Phone Phone { get; private set; } = default!;
+    public Dni Dni { get; private set; } = default!;
+    public Email Email { get; private set; } = default!;
+    public Password Password { get; private set; } = default!;
+    public Role Role { get; private set; } = default!;
+
+
+    public static User Create(
+            Guid id,
+            Name name,
+            Phone phone,
+            Dni dni,
+            Email email,
+            Password password,
+            Role role)
+    {
+        var user = new User
+        {
+            Id = id,
+            Name = name,
+            Phone = phone,
+            Dni = dni,
+            Email = email,
+            Password = password,
+            Role = role
+        };
+
+        user.AddDomainEvent(new UserCreatedEvent(user));
+
+        return user;
+    }
+
+    public void Update(Name name, Phone phone, Email email, Password password, Role role)
+    {
+        Name = name;
+        Phone = phone;
+        Email = email;
+        Password = password;
+        Role = role;
+
+        AddDomainEvent(new UserUpdatedEvent(this));
+    }
+}
+}
---
+++ src/Services/Users/Users.Domain/ValueObjects/Name.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Users.Domain.ValueObjects;
+
+public record Name
+{
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    private Name(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Name Of(string firstName, string lastName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(firstName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(lastName);
+    
+        return new Name(firstName, lastName);
+    }
+}
