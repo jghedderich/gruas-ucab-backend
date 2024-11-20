4f230aa83e53e6ecdcbab16910c262a54482d498
Droski0802 <pedromanuelwork@gmail.com>
2024-11-20 15:17:52 -0400

    Comandos de Operador (Update y Delete)

---
+++ src/Services/Orders/Orders.API/Endpoints/Operators/DeleteOperator.cs
@@ -0,0 +1,26 @@
+﻿using Orders.Application.Operators.Commands.DeleteOperator;
+
+namespace Orders.API.Endpoints.Operators;
+
+public record DeleteOperatorResponse(bool IsSuccess);
+
+public class DeleteOperator : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/operators/{id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOperatorCommand(Id));
+
+            var response = result.Adapt<DeleteOperatorResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOperator")
+        .Produces<DeleteOperatorResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Operator")
+        .WithDescription("Delete Operator");
+    }
+}
---
+++ src/Services/Orders/Orders.Application/Operators/Commands/DeleteOperator/DeleteOperatorHandler.cs
@@ -0,0 +1,19 @@
+﻿
+using Orders.Application.Exceptions;
+
+namespace Orders.Application.Operators.Commands.DeleteOperator;
+
+public class DeleteOperatorHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOperatorCommand, DeleteOperatorResult>
+{
+    public async Task<DeleteOperatorResult> Handle(DeleteOperatorCommand command, CancellationToken cancellationToken)
+    {
+        var operatorN = await dbContext.Operators
+            .FindAsync([command.OperatorId], cancellationToken)
+            ?? throw new OperatorNotFoundException(command.OperatorId);
+
+        dbContext.Operators.Remove(operatorN);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOperatorResult(true);
+    }
+}
