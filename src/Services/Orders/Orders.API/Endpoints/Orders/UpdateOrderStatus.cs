787648b0a59382390c9185061574c39a7a27c0c9
Droski0802 <pedromanuelwork@gmail.com>
2024-11-24 12:59:24 -0400

    feat(orders): add update status order

---
+++ src/Services/Orders/Orders.API/Endpoints/Orders/UpdateOrderStatus.cs
@@ -0,0 +1,29 @@
+﻿using Orders.Application.Dtos;
+using Orders.Application.Orders.Commands.UpdateOrderStatus;
+
+namespace Orders.API.Endpoints.Orders;
+
+public record UpdateOrderStatusRequest(UpdateStatusDto Order);
+public record UpdateOrderStatusResponse(bool IsSuccess);
+
+public class UpdateOrderStatus : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders/status", async (UpdateOrderStatusRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateOrderStatusCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateOrderStatusResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateOrderStatus")
+        .Produces<UpdateOrderStatusResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Order Status")
+        .WithDescription("Update Order Status");
+    }
+}
---
+++ src/Services/Orders/Orders.Application/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusHandler.cs
@@ -0,0 +1,32 @@
+﻿using Orders.Application.Exceptions;
+
+namespace Orders.Application.Orders.Commands.UpdateOrderStatus;
+
+public class UpdateOrderStatusHandlerI(IApplicationDbContext dbContext)
+    : ICommandHandler<UpdateOrderStatusCommand, UpdateOrderStatusResult>
+{
+    public async Task<UpdateOrderStatusResult> Handle(UpdateOrderStatusCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = command.Order.Id;
+        var status = command.Order.OrderStatus;
+        var order = await dbContext.Orders
+            .FindAsync([orderId], cancellationToken: cancellationToken) ?? throw new OrderNotFoundException(command.Order.Id);
+
+
+        UpdateOrderStatus(order, status);
+
+        dbContext.Orders.Update(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateOrderStatusResult(true);
+    }
+
+    public static void UpdateOrderStatus(Order order, string status)
+    {
+        if (Enum.TryParse<Status>(status, out Status statusEnum))
+        {
+            order.UpdateStatus(OrderStatus.Of(statusEnum));
+        }
+
+    }
+}
