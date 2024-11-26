442b6ec28bf0886aa81cd0230b2e716c42a4bf83
Droski0802 <pedromanuelwork@gmail.com>
2024-11-26 08:24:41 -0400

    feat(orders): add queries for costdetails

--- src/Services/Orders/Orders.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
+++ src/Services/Orders/Orders.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -11,7 +11,7 @@ public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<G
         var pageIndex = query.PaginationRequest.PageIndex;
         var pageSize = query.PaginationRequest.PageSize;
 
-        var totalCount = await dbContext.Operators.LongCountAsync(cancellationToken);
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
 
         var orders = await dbContext.Orders
             .Include(o => o.CostDetails)
