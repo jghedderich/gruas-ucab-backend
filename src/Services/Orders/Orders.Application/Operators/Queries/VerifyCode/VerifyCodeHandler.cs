4f800348e59ac852c8da31883199d855132bd0d8
Droski0802 <pedromanuelwork@gmail.com>
2024-12-01 14:33:35 -0400

    feat(orders): add recovery password for operators

---
+++ src/Services/Orders/Orders.Application/Operators/Queries/VerifyCode/VerifyCodeHandler.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Caching;
+using BuildingBlocks.Exceptions;
+
+namespace Orders.Application.Operators.Queries.VerifyCode;
+
+public class VerifyCodeHandler(IRedisCacheService redisCacheService) :
+    IQueryHandler<VerifyCodeQuery, VerifyCodeResult>
+
+{
+    public async Task<VerifyCodeResult> Handle(VerifyCodeQuery query, CancellationToken cancellationToken)
+    {
+        var cachingKey = $"code_{query.Code}";
+        var data = redisCacheService.GetData<VerifyCodeDto>(cachingKey)
+            ?? throw new NotFoundException("The code submitted is incorrect");
+
+        redisCacheService.DeleteData(cachingKey);
+
+        var verifyDto = new VerifyCodeDto(IsSuccess: true, Id: data.Id);
+
+        return new VerifyCodeResult(verifyDto);
+    }
+}
