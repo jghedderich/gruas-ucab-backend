eb7b3d6bda60253809b6319e4aa14b49ef422280
Droski0802 <pedromanuelwork@gmail.com>
2024-12-21 14:31:04 -0400

    feature(admin): added admin can send notifications

---
+++ src/Services/Admin/Admin.API/Endpoints/SendNotification.cs
@@ -0,0 +1,50 @@
+﻿using Admin.Infrastructure.Settings;
+using FirebaseAdmin.Messaging;
+
+namespace Admin.API.Endpoints;
+
+public record SendNotificationRequest(string DeviceToken, string Title, string Body);
+public record SendNotificationResponse(bool IsSuccess, string? ErrorMessage);
+
+public class SendNotification : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+
+        // Create a message
+       // var message = new Message()
+       // {
+         //   Token = "773576465708-60rikb1viqdgfgcipvka4tluct1po87s.apps.googleusercontent.com", // Replace with the device token you get from the Firebase Console
+          //  Notification = new Notification()
+           // {
+            //    Title = "Test Notification",
+            //    Body = "This is a test notification."
+           // }
+     //   };
+
+        // Send the message
+        // string response = FirebaseMessaging.DefaultInstance.SendAsync(message).Result;
+        // Console.WriteLine("Successfully sent message: " + response);
+
+        app.MapPost("/notifications/send", async (SendNotificationRequest request, IFirebaseMessagingService messagingService) =>
+        {
+
+            try
+            {
+                await messagingService.SendPushNotificationAsync(request.DeviceToken, request.Title, request.Body);
+                var response = new SendNotificationResponse(true, null);
+                return Results.Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new SendNotificationResponse(false, ex.Message);
+                return Results.Problem(response.ErrorMessage, statusCode: StatusCodes.Status500InternalServerError);
+            }
+        })
+        .WithName("SendNotification")
+        .Produces<SendNotificationResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithSummary("Send Notification")
+        .WithDescription("Send a push notification to a specific device using Firebase.");
+    }
+}
---
+++ src/Services/Admin/Admin.Infrastructure/Settings/FirebaseMessagingSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Admin.Infrastructure.Settings;
+
+public class FirebaseMessagingSettings
+{
+    public string ChannelId { get; set; } = "default_channel";
+    public string MessageSound { get; set; } = "default";   
+}
