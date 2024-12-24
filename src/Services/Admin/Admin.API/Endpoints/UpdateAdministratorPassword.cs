9e7db75ad467c1f8ef3dcc53072ec23798d1d2a5
Juan Hedderich <hedderich.dev@gmail.com>
2024-12-24 17:33:36 -0400

    update admin password, fix admin authenticate and request code (#73)

--- src/Services/Admin/Admin.API/Endpoints/UpdateAdministratorPassword.cs
+++ src/Services/Admin/Admin.API/Endpoints/UpdateAdministratorPassword.cs
@@ -3,27 +3,27 @@ using Admin.Application.Administrators.Commands.UpdateAdministratorPassword;
 
 namespace Admin.API.Endpoints;
 
-public record UpdateAdmnistratorPasswordRequest(UpdatePasswordDto Administrator);
-public record UpdateAdmnistratorPasswordResponse(bool IsSuccess);
+public record UpdateAdministratorPasswordRequest(UpdatePasswordDto Administrator);
+public record UpdateAdministratorPasswordResponse(bool IsSuccess);
 
-public class UpdateAdmnistratorPassword : ICarterModule
+public class UpdateAdministratorPassword : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPut("/administrators/password", async (UpdateAdmnistratorPasswordRequest request, ISender sender) =>
+        app.MapPut("/administrators/password", async (UpdateAdministratorPasswordRequest request, ISender sender) =>
         {
             var command = request.Adapt<UpdateAdministratorPasswordCommand>();
 
             var result = await sender.Send(command);
 
-            var response = result.Adapt<UpdateAdmnistratorPasswordResponse>();
+            var response = result.Adapt<UpdateAdministratorPasswordResponse>();
 
             return Results.Ok(response);
         })
-        .WithName("UpdateAdmnistratorPassword")
-        .Produces<UpdateAdmnistratorPasswordResponse>(StatusCodes.Status200OK)
+        .WithName("UpdateAdministratorPassword")
+        .Produces<UpdateAdministratorPasswordResponse>(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Update Admnistrator Password")
-        .WithDescription("Update Admnistrator Password");
+        .WithSummary("Update Administrator Password")
+        .WithDescription("Update Administrator Password");
     }
 }
--- src/Services/Admin/Admin.Application/Administrators/Commands/UpdateAdministratorPassword/UpdateAdministratorPasswordCommand.cs
+++ src/Services/Admin/Admin.Application/Administrators/Commands/UpdateAdministratorPassword/UpdateAdministratorPasswordCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Admin.Application.Administrators.Commands.UpdateAdministratorPassword;
+﻿namespace Admin.Application.Administrators.Commands.UpdateAdministratorPassword;
 
 public record UpdateAdministratorPasswordCommand(UpdatePasswordDto Administrator)
     : ICommand<UpdateAdministratorPasswordResult>;
