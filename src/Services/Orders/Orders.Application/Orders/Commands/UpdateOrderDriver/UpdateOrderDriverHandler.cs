7bb3d78bcab465c0b09a13b8ea019ad3bd932614
Droski0802 <pedromanuelwork@gmail.com>
2025-01-10 15:20:07 -0400

    feat(orders): :sparkles: added update order driver

---
+++ src/Services/Orders/Orders.Application/Orders/Commands/UpdateOrderDriver/UpdateOrderDriverHandler.cs
@@ -0,0 +1,27 @@
+﻿
+using Orders.Application.Exceptions;
+
+namespace Orders.Application.Orders.Commands.UpdateOrderDriver;
+
+public class UpdateOrderDriverHandler(IApplicationDbContext dbContext) : ICommandHandler<UpdateOrderDriverCommand, UpdateOrderDriverResult>
+{
+    public async Task<UpdateOrderDriverResult> Handle(UpdateOrderDriverCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = command.Order.Id;
+        var driverId = command.Order.DriverId;
+        var order = await dbContext.Orders
+            .FindAsync([orderId], cancellationToken: cancellationToken) ?? throw new OrderNotFoundException(command.Order.Id);
+
+        UpdateOrderDriver(order, driverId);
+
+        dbContext.Orders.Update(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateOrderDriverResult(true);
+    }
+
+    public static void UpdateOrderDriver(Order order, Guid driverId)
+    {
+        order.UpdateOrderDriver(driverId);
+    }
+}
