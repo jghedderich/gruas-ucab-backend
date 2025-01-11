e50bc1caf21ffbca48198cac76be5e9bd0c7afd3
Juan Hedderich <hedderich.dev@gmail.com>
2025-01-11 13:25:41 -0400

    increase jwt expiration time to 24h and create driverAssigned integration event

---
+++ src/BuildingBlocks/BuildingBlocks/Firebase/FcmMessage.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace BuildingBlocks.Firebase;
+
+public class FcmMessage
+{
+    public string To { get; set; } = default!;
+    public string Priority { get; set; } = default!;
+    public Notification Notification { get; set; } = default!;
+}
+
+public class Notification
+{
+    public string Title { get; set; } = default!;
+    public string Body { get; set; } = default!;
+}
---
+++ src/Services/Providers/Providers.Application/Drivers/EventHandlers/Integration/DriverAssignedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Providers.Application.Drivers.Commands.AssignDriver;
+
+namespace Providers.Application.Drivers.EventHandlers.Integration
+{
+
+    public class DriverAssignedEventHandler
+    (ISender sender, ILogger<DriverAssignedEventHandler> logger)
+    : IConsumer<DriverAssignedEvent>
+    {
+        public async Task Consume(ConsumeContext<DriverAssignedEvent> context)
+        {
+            // Update order status
+            logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+            var command = MapToAssignDriverCommand(context.Message);
+            await sender.Send(command);
+        }
+
+        private static AssignDriverCommand MapToAssignDriverCommand(DriverAssignedEvent message)
+        {
+            var updateStatusDto = new AssignDriverDto(message.OrderId, message.DriverId);
+            return new AssignDriverCommand(updateStatusDto);
+        }
+    }
+}
