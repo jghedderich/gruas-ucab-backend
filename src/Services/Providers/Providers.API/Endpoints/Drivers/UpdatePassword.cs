e5e724eeb27ca5b4767513a989370529015ddf98
Droski0802 <pedromanuelwork@gmail.com>
2025-01-15 14:09:04 -0400

    fix(providers): :ambulance: Fixed drivers update password endpoint

--- src/Services/Providers/Providers.API/Endpoints/Drivers/UpdatePassword.cs
+++ src/Services/Providers/Providers.API/Endpoints/Drivers/UpdatePassword.cs
@@ -23,7 +23,6 @@ public class UpdateDriverPassword : ICarterModule
         .Produces<UpdateDriverPasswordResponse>(StatusCodes.Status200OK)
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Update Driver Password")
-        .WithDescription("Update Driver Password")
-        .RequireAuthorization();
+        .WithDescription("Update Driver Password");
     }
 }
